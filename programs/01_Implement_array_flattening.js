fedb4f1135b910e5905c8b7ddd0ef7120d5e371f
snehaprakas1h <snehakp121@gmail.com>
2024-09-10 07:06:03 +0100

    first commit

---
+++ programs/01_Implement_array_flattening.js
@@ -0,0 +1,18 @@
+function flattenArrayRecursive(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])){
+            result = result.concat(flattenArrayRecursive(arr[i]));
+        }
+        else{
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+const input = [1, [2, 3], [4, [5, 6]], 7];
+
+console.log(flattenArrayRecursive(input));
+
